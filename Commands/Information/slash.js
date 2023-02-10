0d36dc4f87e174d6d086935a49aab68f2d47e009
Sarthak Kundu <74956651+dragoluca22@users.noreply.github.com>
2023-02-10 18:42:08 +0530

    Added scb guide

---
+++ Commands/Information/slash.js
@@ -0,0 +1,59 @@
+const { SlashCommandBuilder, ChatInputCommandInteraction, ChannelType } = require("discord.js");
+const { CustomClient } = require("../../Structures/Classes/CustomClient");
+const Reply = require("../../Systems/Reply");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("slash")
+        .setDescription("This is a slash command")
+
+        .addSubcommand(sub => sub.setName("slice").setDescription("This is a slice").addStringOption(opt => opt.setName("string").setDescription("Provide a string").setRequired(true)))
+        .addSubcommandGroup(grp => grp.setName("parts").setDescription("These are the commands").addSubcommand(sub => sub.setName("part").setDescription("This is a part").addStringOption(opt => opt.setName("string").setDescription("Provide a string").setRequired(true))))
+
+        // .addAttachmentOption(opt => opt.setName("attachment").setDescription("Add an attachment").setRequired(true))
+        // .addChannelOption(opt => opt.setName("channel").setDescription("Provide a channel").setRequired(true).addChannelTypes(ChannelType.GuildText, ChannelType.GuildAnnouncement))
+        // .addIntegerOption(opt => opt.setName("integer").setDescription("Provide an integer").setRequired(true).setMaxValue(200).setMinValue(20))
+        // .addNumberOption(opt => opt.setName("number").setDescription("Provide a number").setRequired(true).setMaxValue(200).setMinValue(20))
+        // .addRoleOption(opt => opt.setName("role").setDescription("Provide a role").setRequired(true))
+        // .addStringOption(opt => opt.setName("string").setDescription("Provide a string").setRequired(true))
+        // .addStringOption(opt => opt.setName("choices").setDescription("Provide a string").setRequired(true).addChoices({ name: "Choice 1", value: "1" }, { name: "Choice 2", value: "2" }, { name: "Choice 3", value: "3" }))
+        // .addUserOption(opt => opt.setName("user").setDescription("Provide a user").setRequired(true))
+        ,
+    owner: true,
+
+    /**
+     * @param {ChatInputCommandInteraction} interaction
+     * @param {CustomClient} client
+     */
+    execute(interaction, client) {
+
+        const { options } = interaction
+
+        const subGrp = options.getSubcommandGroup()
+        const sub = options.getSubcommand()
+
+        const attachment = options.getAttachment("attachment")
+        const channel = options.getChannel("channel")
+        const int = options.getInteger("integer")
+        const num = options.getNumber("number")
+        const role = options.getRole("role")
+        const str = options.getString("string")
+        const opt = options.getString("choices")
+        const user = options.getUser("user")
+        const member = options.getMember("user")
+
+        switch (sub) {
+
+            case "slice": {
+
+                // your code
+
+            }
+                break;
+
+        }
+
+        Reply(interaction, client.emojiList.tick, `This is a slash command!`)
+
+    }
+}
